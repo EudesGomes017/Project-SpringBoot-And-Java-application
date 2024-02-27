2591e0aa23a4b313ab04d7bd363e61a41426ef68
EudesGomes017 <eudesgomes017@gmail.com>
2024-02-27 15:37:35 -0300

    Create class Category

---
+++ src/main/java/com/dlea/Course/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.dlea.Course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dlea.Course.entities.Category;
+import com.dlea.Course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	
+	public List<Category> finAll() {
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		
+		 Optional<Category> obj = categoryRepository.findById(id);
+		 return obj.get();
+	}
+
+}
