9e87d26819f38da2fba3fa363b15425dd18292bf
EudesGomes017 <eudesgomes017@gmail.com>
2024-02-27 22:58:53 -0300

    Create association in between Order, OrderItems and Product with compounds

--- src/main/java/com/dlea/Course/config/TestConfig.java
+++ src/main/java/com/dlea/Course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.dlea.Course.entities.Category;
 import com.dlea.Course.entities.Order;
+import com.dlea.Course.entities.OrderItem;
 import com.dlea.Course.entities.Product;
 import com.dlea.Course.entities.User;
 import com.dlea.Course.entities.enums.OrderStatus;
 import com.dlea.Course.repositories.CategoryRepository;
+import com.dlea.Course.repositories.OrderItemRepository;
 import com.dlea.Course.repositories.OrderRepository;
 import com.dlea.Course.repositories.ProductRepository;
 import com.dlea.Course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -72,6 +77,13 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1, u2));
 
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 	}
 
