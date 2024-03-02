7292dc7366340da677a36ac46b3d413305d41ac8
EudesGomes017 <eudesgomes017@gmail.com>
2024-03-02 09:24:11 -0300

    Create Association, in Product, Order, OrderItem

--- src/main/java/com/dlea/Course/entities/Product.java
+++ src/main/java/com/dlea/Course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,11 +33,14 @@ public class Product implements Serializable {
 
 	// Utilizamos Set por que ele é um conjunto para coleção por que não queremos
 	// que nosso produtos tenha mais de uma ocorrencia da mesma categoria
-	//associação dos produtos com as categorias muitos para muitos
+	// associação dos produtos com as categorias muitos para muitos
 	@ManyToMany
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();// instaciamos para nossa coleção não começar null, ela começa
 														// vazia porem instanciada
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 
@@ -91,6 +97,15 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+
+		return set;
 	}
 
 	@Override
